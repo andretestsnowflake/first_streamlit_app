e5cb490480a17ea29fbbfb7194de04ebc21063c5
andretestsnowflake <113890451+andretestsnowflake@users.noreply.github.com>
2022-09-19 17:28:37 +0200

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -6,9 +6,9 @@ import requests
 
 # put choice into variable
 Pokemon_name_search = streamlit.text_input('Which POKEMON you are looking for?','Charizar')
-
+api_key="1b7fbe88-8707-4a11-bde2-b47ddcec5c10"
 q="!name:Charizar"
-Search_response = requests.get("https://api.pokemontcg.io/v2/cards/"+q)
+Search_response = requests.get("https://api.pokemontcg.io/v2/cards/"+q+api_key)
 Pokemon_data_normalized = pandas.json_normalize(Search_response.json())
 streamlit.dataframe(Pokemon_data_normalized)
 
